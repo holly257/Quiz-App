d673c79e504c10c7166552f9b77baea3a187a497
Holly Rogers <hollyrogers@Hollys-Air.lan>
2019-12-16 09:30:48 -0500

    started slideshow formatting

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+
+//slideshow js
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+  
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i<slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000);
+}
+
+//https://www.w3schools.com/howto/howto_js_slideshow.asp
+//https://www.htmlgoodies.com/beyond/javascript/stips/create-a-simple-automated-slideshow.html
