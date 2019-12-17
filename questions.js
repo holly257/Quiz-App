665e6d83ba02f9b6f5c159cfdf07e694eb8cd103
Holly Rogers <hollyrogers@Hollys-Air.lan>
2019-12-17 09:22:47 -0500

    added questions.js

---
+++ questions.js
@@ -0,0 +1,84 @@
+const store = {
+    questions: [ 
+        //1
+        {
+            question: "What is the largest archipelago, or chain of islands, in the world?",
+            options: [
+                "Norwegian Archipelago",
+                "Archipelago Sea between Finland and the Baltic Sea",
+                "Canadian Arctic Archipelago",
+                "Stockholm Archipelago",
+                "Indonesian Archipelago"
+            ],
+            answer: "Norwegian Archipelago"
+        },
+        //2
+        {
+            question: "Lions Head is location in what country?",
+            options: [
+                "Madagascar",
+                "India",
+                "South Africa",
+                "Tanzania"
+            ],
+            answer: "South Africa"
+        },
+        //3
+        {
+            question: "Which body of water is on the East of Italy?",
+            options: [
+                "Mediterranean Sea",
+                "Adriatic Sea",
+                "Caspian Sea",
+                "Persian Gulf"
+            ],
+            answer: "Adriatic Sea"
+        },
+        //4
+        {
+            question: "The Himalayas go through which country?",
+            options: [
+                "Nepal",
+                "Tanzania",
+                "Switzerland",
+                "Russia"
+            ],
+            answer: "Nepal"
+        },
+        //5
+        {
+            question: "Kosovo used to be a part of which other country?",
+            options: [
+                "None, they have always been independant",
+                "Bosnia",
+                "Russia",
+                "Serbia"
+            ],
+            answer: "Serbia"
+        },
+        //6
+        {
+            question: "Someone who is of the Khmer ethnicity would most likely be from:",
+            options: [
+                "Myanmar",
+                "Cambodia",
+                "Malaysia",
+                "Thailand"
+            ],
+            answer: "Cambodia"
+        },
+        //7
+        {
+            question: "The largest city in Vietnam, Ho Chi Minh, used to be called:",
+            options: [
+                "Hanoi",
+                "It has always been called Ho Chi Minh",
+                "Phnom Penh",
+                "Saigon"
+            ],
+            answer: "Saigon"
+        },
+    ],
+    currentQuestion: 0;
+    score: 0;
+};
