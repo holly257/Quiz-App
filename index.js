6a8f02004a6a31058c88453f4b94d1c04e337056
Holly Rogers <hollyrogers@Hollys-Air.lan>
2019-12-17 09:10:28 -0500

    slides change every 4 seconds

--- index.js
+++ index.js
@@ -9,19 +9,21 @@ function currentSlide(n) {
 
 var slideIndex = 1;
 showSlides(slideIndex);
-  
+
 
 function showSlides() {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("slides");
     for (i = 0; i<slides.length; i++) {
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000);
+    setTimeout(showSlides, 4000);
 }
 
+
+
 //https://www.w3schools.com/howto/howto_js_slideshow.asp
-//https://www.htmlgoodies.com/beyond/javascript/stips/create-a-simple-automated-slideshow.html
+//https://www.htmlgoodies.com/beyond/javascript/stips/create-a-simple-automated-slideshow.html 
